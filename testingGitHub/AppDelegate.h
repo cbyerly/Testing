f345116a7a57e90652c3e56c12b048f9d7929a5d
Cheryl Byerly <cbyerly@Cheryls-MacBook-Pro.local>
2013-05-23 18:13:08 -0500

    Initial Commit

---
+++ testingGitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testingGitHub
+//
+//  Created by Cheryl Byerly on 5/23/13.
+//  Copyright (c) 2013 Cheryl Byerly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testingGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testingGitHub
+//
+//  Created by Cheryl Byerly on 5/23/13.
+//  Copyright (c) 2013 Cheryl Byerly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
